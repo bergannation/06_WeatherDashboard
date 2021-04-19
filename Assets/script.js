444d70ac51184300751b215311185a3ae610e616
Travis Brown <bergannation@gmail.com>
2021-04-19 00:03:43 -0500

    built starting HTML code and small JS

---
+++ Assets/script.js
@@ -0,0 +1,18 @@
+//
+const API_KEY = "m39euATa9KeZDVwEi4Mejq1Wfbvgfthw";
+const IMG_PER_PAGE = 20;
+
+// Elements
+const topDiv = $("#topDiv");
+const bottomDiv = $("#bottomDiv");
+const sideDiv = $("#sideDiv");
+
+// Variables
+var Cities = [
+  "Omaha",
+  "New York City",
+  "Boston",
+  "Kansas City",
+  "Los Angeles",
+  "Houston",
+];
