07a01980bf91ecc50c59a72f1ecd16d58816db9c
Travis Brown <bergannation@gmail.com>
2021-04-19 14:44:29 -0500

    working on Fetch API

--- Assets/script.js
+++ Assets/script.js
@@ -20,7 +20,7 @@ var Cities = [
 ];
 var NUMBER_OF_CITIES = Cities.length;
 
-function RenderTopics() {
+function renderButtons() {
   //
   var topic;
   var rowEl, colEl, btnEl;
@@ -30,18 +30,19 @@ function RenderTopics() {
   rowEl = $("<div>");
   // rowEl.addClass("row row-custom");
   rowEl.addClass("row-cols-auto p-10 m-10");
+  rowEl.attr("id", "cities");
   //
   for (var i = 0; i < Cities.length; i++) {
     //
     topic = Cities[i];
     //
     colEl = $("<div>");
-    // colEl.addClass("col-md-2 col-sm-3 col-4 col-custom");
+    // colEl.addClass
     colEl.addClass("col-6 p-1");
     //
     btnEl = $("<button>");
     btnEl.text(topic);
-    btnEl.attr("image-text", topic);
+    btnEl.attr("id", topic);
     btnEl.addClass("btn btn-light text-capitalize");
     btnEl.appendTo(colEl);
     //
@@ -52,9 +53,57 @@ function RenderTopics() {
   rowEl.appendTo(buttonDiv);
   //
 }
+function renderImage(data) {
+  //
+  var rowEl, colEl, imgEl, pEl;
+  var fixedImg, animatedImg;
+  //
+  topDiv.empty();
+  // console.log(data);
+  //
+  rowEl = $("<div>");
+  // rowEl.addClass("row d-flex flex-wrap");
+  rowEl.addClass("d-flex align-content-stretch flex-wrap row row-cols-auto");
+  //
+  for (var i = 0, l = data.length; i < l; i++) {
+    //
+    // fixedImg = data[i].images.fixed_height_small_still.url;
+    // animatedImg = data[i].images.original.url;
+    fixedImg = data[i].images.original_still.url;
+    animatedImg = data[i].images.original.url;
 
-// Set the Giphy API call
-function ProcessSearch() {
+    // Create a colum
+    colEl = $("<div>");
+    // colEl.addClass("col-md-3 col-sm-3 col-4 text-center col-custom");
+    colEl.addClass("col text-center col-custom");
+    colEl.attr("col-number");
+    //
+    // Create an image
+    imgEl = $("<img>");
+    imgEl.attr("fixed", fixedImg);
+    imgEl.attr("animated", animatedImg);
+    imgEl.attr("state", "fixed");
+    imgEl.attr("src", fixedImg);
+    // imgEl.attr("");
+    // imgEl.addClass("img-fluid img-thumbnail");
+    imgEl.addClass("img-fluid shadow-lg d-block");
+    imgEl.appendTo(colEl);
+    //
+    pEl = $("<p>");
+    pEl.addClass("text-uppercase p-custom");
+    pEl.text(data[i].rating);
+    pEl.appendTo(colEl);
+    //
+    colEl.appendTo(rowEl);
+    //
+  }
+  //
+  rowEl.appendTo(topDiv);
+  //
+}
+
+// Set the Weather API call
+function processSearch() {
   //
 
   //
@@ -62,19 +111,49 @@ function ProcessSearch() {
   //
   if (searchText != "") {
     //
-    var apiUrl = "https://openweathermap.org/find";
+    var apiUrl = "api.openweathermap.org/data/2.5/weather";
     apiUrl = apiUrl + "?q=" + searchText;
-    apiUrl = apiUrl + "&api_key=" + API_KEY;
+    apiUrl = apiUrl + "&appid=" + API_KEY;
     //
-    AddCity(searchText);
-    RenderTopics();
-    console.log(apiUrl);
+    // getWeather function is called with apiUrl passed into it
+    getWeather(apiUrl);
+    // we add a city to our button list
+    addCity(searchText);
+    // we render the buttons to show the cities
+    renderButtons();
     //
   }
   //
 }
+// Call the Giphy API
+function getWeather(apiUrl) {
+  //
+  fetch(apiUrl)
+    //
+    .then(function (response) {
+      //
+      if (response.ok) {
+        //
+        response.json().then(function (res) {
+          //
+          console.log(res.data);
+          //
+        });
+      } else {
+        //
+        alert("Error: " + response.statusText);
+        //
+      }
+    })
+    .catch(function (error) {
+      //
+      alert("Unable to connect to Weather Dashboard");
+      //
+    });
+  //
+}
 // Adds topic to the list
-function AddCity(searchCity) {
+function addCity(searchCity) {
   //
   if (!(Cities.indexOf(searchCity) >= 0)) {
     //
@@ -87,5 +166,5 @@ function AddCity(searchCity) {
 }
 
 // buttonDiv.on("click", "button", ProcessButton);
-searchButton.on("click", ProcessSearch);
-RenderTopics();
+searchButton.on("click", processSearch);
+renderButtons();
